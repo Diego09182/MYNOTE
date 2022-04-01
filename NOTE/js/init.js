562844419c1f2d4fc4cc1007037c3af4d787ac0f
張皓明 <63291576+Diego09182@users.noreply.github.com>
2022-04-01 09:56:46 +0800

    Delete NOTE directory

--- NOTE/js/init.js
+++
@@ -1,9 +0,0 @@
-(function($){
-  $(function(){
-
-    $('.button-collapse').sideNav();
-    $('.parallax').parallax();
-	AOS.init();
-	$('.modal').modal();
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
